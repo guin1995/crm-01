74874ef62a31487f03970912a986ac6396ff9ce8
guin1995 <973357721@qq.com>
2016-09-18 21:12:45 +0800

    333
    333

--- src/com/atguigu/pojo/Book.java
+++ src/com/atguigu/pojo/Book.java
@@ -5,6 +5,6 @@ public class Book {
 	public static void main(String[] args) {
 		System.out.println("Book.main()");
                 System.out.println("Book.main()");
-                	System.out.println("Book.main()");
+                	System.out.println("Book.main(111111111)");
 	}
 }
